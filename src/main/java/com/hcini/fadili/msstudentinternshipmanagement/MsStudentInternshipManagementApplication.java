344d64a0c3a2c8cda952229e61674869165da304
FADILI EL MOSTAFA <mustafa.fadili@dakilab.fr>
2022-04-02 13:30:59 +0000

    new commit

--- src/main/java/com/hcini/fadili/msstudentinternshipmanagement/MsStudentInternshipManagementApplication.java
+++ src/main/java/com/hcini/fadili/msstudentinternshipmanagement/MsStudentInternshipManagementApplication.java
@@ -1,7 +1,11 @@
 package com.hcini.fadili.msstudentinternshipmanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class MsStudentInternshipManagementApplication {
@@ -10,6 +14,10 @@ public class MsStudentInternshipManagementApplication {
         SpringApplication.run(MsStudentInternshipManagementApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner start() {
 
+        return args -> System.out.println("hello friends");
+    }
 
 }
